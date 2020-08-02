5e5072a96832e123ac98be5dea2ae7a251d0e1d7
Zemian Deng <zemian.deng@oracle.com>
2020-08-02 18:27:14 -0400

    Add learn-jekyll/generate-posts.py

---
+++ learn-jekyll/generate-posts.py
@@ -0,0 +1,23 @@
+import sys
+from datetime import date, timedelta
+
+dir = sys.argv[1]
+start_date = date(2020, 1, 1)
+
+def create_post(seq):
+	fdate = (start_date - timedelta(days=seq)).strftime("%Y-%m-%d")
+	fname = "{}/{}-test-{}.md".format(dir, fdate, seq)
+	print(f"Generating file: {fname}")
+	with open(fname, 'w') as f:
+		f.write(f"""---
+layout: post
+title:  "Bulk Test #{seq}"
+date:   {fdate}
+categories: test
+---
+
+> This is test #{seq}
+""")
+
+for i in range(1, 1000):
+	create_post(i)
